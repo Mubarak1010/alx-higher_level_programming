7851f2dfaf94bfeef5f3d8a539681601393c9a43
Mubarak1010 <anoze941@gmail.com>
2023-08-28 04:00:07 -0700

    second commit

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    try:
-        for i in x:
-            print("{:d}".format(my_list[i]))
-    except:
-        print("Cannot print")
+    j = 0
+    for i in range(x):
+        try:
+            print("{}".format(my_list[i]), end="")
+            j += 1
+        except IndexError:
+            break
 
-    return (x)
+    print("")
+    return (j)
--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,17 +1,12 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
     count = 0
-    j = 0
-    for i in my_list:
-        count = count + 1
-
-    try:
-        if x < count or x == count:
-            for j in range(x):
-                print("{:d}".format(my_list[j]), end="")
-
-    except IndexError:
-        print("list index out of range")
-
-    print()
-    return (x)
+    
+    for i in range(x):
+        try:
+            print("{:d}".format(my_list[i]), end="")
+            count += 1
+        except:
+            continue
+    print("")
+    return (count)
