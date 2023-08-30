3dba82183df7c87f56af671dd521ab426ef0fa80
Mubarak1010 <anoze941@gmail.com>
2023-08-30 01:22:12 -0700

    second commit

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-for i in range(9):
-    for j in range(1, 10):
+for i in range(0, 9):
+    for j in range(i + 1, 10):
         if i != 8:
             print("{}{}, ".format(i, j), end="")
         else:
