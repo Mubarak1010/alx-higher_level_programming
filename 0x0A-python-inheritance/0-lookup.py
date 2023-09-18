9552493b731f510bbffbc617aa701e46f8431979
Mubarak1010 <anoze941@gmail.com>
2023-09-18 05:27:18 -0700

    first commit

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+"""Module."""
+
+def lookup(obj):
+    """Method declaration."""
+    return (dir(obj))
---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Module."""
+
+def is_same_class(obj, a_class):
+    """Class declaration."""
+    if isinstance(obj, a_class):
+        return (True)
+
+    return (False)
