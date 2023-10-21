d83c9830dfeeb9eba7d7b05ad27cf73a0f4f2da7
Mubarak1010 <anoze941@gmail.com>
2023-10-20 23:08:17 -0700

    third commit

--- 0x0D-SQL_introduction/0-list_databases.sql
+++ 0x0D-SQL_introduction/0-list_databases.sql
@@ -1,2 +1,2 @@
--- lists all databases of your MySQL server
-show databases;
+-- lists all databases of your MySQL server.
+SHOW DATABASES;
---
+++ 0x0D-SQL_introduction/14-average.sql
@@ -0,0 +1,3 @@
+-- computes the score average of all records in the table
+SELECT SUM(score / 3) AS average
+FROM second_table;
