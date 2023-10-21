5957a3f0ca2694c47a44e71bf34fb74aeae3ed19
Mubarak1010 <anoze941@gmail.com>
2023-10-21 01:48:14 -0700

    fourth commit

--- 0x0D-SQL_introduction/12-no_cheating.sql
+++ 0x0D-SQL_introduction/12-no_cheating.sql
@@ -1,4 +1,4 @@
 -- updates the score of Bob to 10 in the table
 UPDATE second_table
 SET score = 10
-WHERE id = 3
+WHERE name = "Bob"
--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,4 +1,5 @@
 -- lists the number of records with the same score in the table
-SELECT score COUNT (score) AS number
+SELECT score, COUNT(*) AS number
 FROM second_table
-ORDER BY score
+GROUP BY score
+ORDER BY number DESC;
